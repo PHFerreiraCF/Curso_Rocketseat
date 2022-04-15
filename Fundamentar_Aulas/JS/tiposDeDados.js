789b218f687a45c9a46e0fb5d14382302e417504
PHFerreiraCF <phferreiracf@gmail.com>
2022-04-14 21:22:37 -0300

    Boolean, Object e Array

---
+++ Fundamentar_Aulas/JS/tiposDeDados.js
@@ -0,0 +1,23 @@
+/*
+Boolean
+
+ *somente 2 valores 
+  true
+  false
+
+  */
+console.log(false)
+console.log(true)
+
+/* =======object======*/
+
+console.log({
+  name: 'Paulo',
+  idade: 25,
+  andar: function () {
+    console.log('andar')
+  }
+})
+
+//=====ARRAY======//
+console.log(['leite', 'ovos'])
